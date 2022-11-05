324ccea9dce0a0eac03945a2b7e98c9ffe83de73
Naveen <adith.naveen@gmail.com>
2022-11-05 13:02:43 +0530

    work on get record user

--- application/js/script.js
+++ application/js/script.js
@@ -23,6 +23,10 @@ function fnAddUser(evt) {
 
 }
 
-function getUserById(evt){
-
+function getUserById(evt) {
+    let userId = document.querySelector("#id").value;
+    fetch("http://localhost:3000/users/" + userId)
+        .then(resp => resp.json())
+        .then(data => console.log(data));
+    // then render on the screen 
 }
